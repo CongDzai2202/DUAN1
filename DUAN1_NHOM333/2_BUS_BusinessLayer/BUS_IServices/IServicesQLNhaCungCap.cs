5c4b7ff0837d95b654cc2fc1c324efb9c6716531
zukong1204 <congph14305@fpt.edu.vn>
2021-11-15 19:10:12 +0700

    load đang lỗi

---
+++ DUAN1_NHOM333/2_BUS_BusinessLayer/BUS_IServices/IServicesQLNhaCungCap.cs
@@ -0,0 +1,18 @@
+﻿using _1_DAL_DataAccesLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_BUS_BusinessLayer.BUS_IServices
+{
+    public interface IServicesQLNhaCungCap
+    {
+        public List<NhaCungCap> getLstNhaCungCaps();
+        public string ThemNCC(NhaCungCap ncc);
+        public string SuaNCC(NhaCungCap ncc);
+        public string saveNCC();
+        public bool Statusdata();
+    }
+}
