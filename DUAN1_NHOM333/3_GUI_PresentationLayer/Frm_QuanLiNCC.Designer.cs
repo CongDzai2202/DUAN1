4d08758754b4cbf97430ce11acc4800f8b352bbf
zukong1204 <congph14305@fpt.edu.vn>
2021-11-16 01:20:06 +0700

    crud xong

--- DUAN1_NHOM333/3_GUI_PresentationLayer/Frm_QuanLiNCC.Designer.cs
+++ DUAN1_NHOM333/3_GUI_PresentationLayer/Frm_QuanLiNCC.Designer.cs
@@ -32,6 +32,7 @@ namespace _3_GUI_PresentationLayer
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.dgv_NhaCungCap = new System.Windows.Forms.DataGridView();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_luu = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.btn_nhaplai = new System.Windows.Forms.Button();
             this.btn_Them = new System.Windows.Forms.Button();
@@ -48,7 +49,6 @@ namespace _3_GUI_PresentationLayer
             this.btn_thoat = new System.Windows.Forms.Button();
             this.btn_search = new System.Windows.Forms.Button();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.btn_luu = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_NhaCungCap)).BeginInit();
             this.groupBox2.SuspendLayout();
@@ -59,9 +59,11 @@ namespace _3_GUI_PresentationLayer
             // 
             this.groupBox1.Controls.Add(this.dgv_NhaCungCap);
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.groupBox1.Location = new System.Drawing.Point(0, 227);
+            this.groupBox1.Location = new System.Drawing.Point(0, 318);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(825, 223);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(1060, 312);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             // 
@@ -69,10 +71,11 @@ namespace _3_GUI_PresentationLayer
             // 
             this.dgv_NhaCungCap.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgv_NhaCungCap.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dgv_NhaCungCap.Location = new System.Drawing.Point(3, 19);
+            this.dgv_NhaCungCap.Location = new System.Drawing.Point(4, 27);
+            this.dgv_NhaCungCap.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dgv_NhaCungCap.Name = "dgv_NhaCungCap";
             this.dgv_NhaCungCap.RowTemplate.Height = 25;
-            this.dgv_NhaCungCap.Size = new System.Drawing.Size(819, 201);
+            this.dgv_NhaCungCap.Size = new System.Drawing.Size(1052, 280);
             this.dgv_NhaCungCap.TabIndex = 0;
             this.dgv_NhaCungCap.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_NhaCungCap_CellClick);
             // 
@@ -91,17 +94,33 @@ namespace _3_GUI_PresentationLayer
             this.groupBox2.Controls.Add(this.label3);
             this.groupBox2.Controls.Add(this.label1);
             this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.groupBox2.Location = new System.Drawing.Point(3, 12);
+            this.groupBox2.ForeColor = System.Drawing.Color.Yellow;
+            this.groupBox2.Location = new System.Drawing.Point(4, 16);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(622, 222);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Size = new System.Drawing.Size(800, 310);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Thông Tin Chi Tiết";
             // 
+            // btn_luu
+            // 
+            this.btn_luu.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btn_luu.Location = new System.Drawing.Point(495, 252);
+            this.btn_luu.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_luu.Name = "btn_luu";
+            this.btn_luu.Size = new System.Drawing.Size(117, 40);
+            this.btn_luu.TabIndex = 9;
+            this.btn_luu.Text = "Lưu";
+            this.btn_luu.UseVisualStyleBackColor = false;
+            this.btn_luu.Click += new System.EventHandler(this.btn_luu_Click);
+            // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(350, 119);
+            this.label5.Location = new System.Drawing.Point(450, 166);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(63, 21);
             this.label5.TabIndex = 13;
@@ -109,67 +128,78 @@ namespace _3_GUI_PresentationLayer
             // 
             // btn_nhaplai
             // 
-            this.btn_nhaplai.Location = new System.Drawing.Point(502, 180);
+            this.btn_nhaplai.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btn_nhaplai.Location = new System.Drawing.Point(645, 252);
+            this.btn_nhaplai.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btn_nhaplai.Name = "btn_nhaplai";
-            this.btn_nhaplai.Size = new System.Drawing.Size(91, 29);
+            this.btn_nhaplai.Size = new System.Drawing.Size(117, 40);
             this.btn_nhaplai.TabIndex = 10;
             this.btn_nhaplai.Text = "Nhập Lại";
-            this.btn_nhaplai.UseVisualStyleBackColor = true;
+            this.btn_nhaplai.UseVisualStyleBackColor = false;
             this.btn_nhaplai.Click += new System.EventHandler(this.btn_nhaplai_Click);
             // 
             // btn_Them
             // 
-            this.btn_Them.Location = new System.Drawing.Point(144, 180);
+            this.btn_Them.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btn_Them.Location = new System.Drawing.Point(184, 252);
+            this.btn_Them.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btn_Them.Name = "btn_Them";
-            this.btn_Them.Size = new System.Drawing.Size(91, 29);
-            this.btn_Them.TabIndex = 12;
+            this.btn_Them.Size = new System.Drawing.Size(117, 40);
+            this.btn_Them.TabIndex = 7;
             this.btn_Them.Text = "Thêm";
-            this.btn_Them.UseVisualStyleBackColor = true;
+            this.btn_Them.UseVisualStyleBackColor = false;
             this.btn_Them.Click += new System.EventHandler(this.btn_Them_Click);
             // 
             // btn_sua
             // 
-            this.btn_sua.Location = new System.Drawing.Point(266, 180);
+            this.btn_sua.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btn_sua.Location = new System.Drawing.Point(342, 252);
+            this.btn_sua.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btn_sua.Name = "btn_sua";
-            this.btn_sua.Size = new System.Drawing.Size(91, 29);
-            this.btn_sua.TabIndex = 10;
+            this.btn_sua.Size = new System.Drawing.Size(117, 40);
+            this.btn_sua.TabIndex = 8;
             this.btn_sua.Text = "Sửa";
-            this.btn_sua.UseVisualStyleBackColor = true;
+            this.btn_sua.UseVisualStyleBackColor = false;
             this.btn_sua.Click += new System.EventHandler(this.btn_sua_Click);
             // 
             // txt_SDT
             // 
-            this.txt_SDT.Location = new System.Drawing.Point(419, 44);
+            this.txt_SDT.Location = new System.Drawing.Point(538, 61);
+            this.txt_SDT.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txt_SDT.Name = "txt_SDT";
-            this.txt_SDT.Size = new System.Drawing.Size(174, 29);
-            this.txt_SDT.TabIndex = 8;
+            this.txt_SDT.Size = new System.Drawing.Size(223, 29);
+            this.txt_SDT.TabIndex = 4;
             // 
             // txt_MaNcc
             // 
             this.txt_MaNcc.Enabled = false;
-            this.txt_MaNcc.Location = new System.Drawing.Point(98, 44);
+            this.txt_MaNcc.Location = new System.Drawing.Point(126, 61);
+            this.txt_MaNcc.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txt_MaNcc.Name = "txt_MaNcc";
-            this.txt_MaNcc.Size = new System.Drawing.Size(174, 29);
-            this.txt_MaNcc.TabIndex = 7;
+            this.txt_MaNcc.Size = new System.Drawing.Size(223, 29);
+            this.txt_MaNcc.TabIndex = 2;
             // 
             // txt_TenNcc
             // 
-            this.txt_TenNcc.Location = new System.Drawing.Point(98, 116);
+            this.txt_TenNcc.Location = new System.Drawing.Point(126, 163);
+            this.txt_TenNcc.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txt_TenNcc.Name = "txt_TenNcc";
-            this.txt_TenNcc.Size = new System.Drawing.Size(174, 29);
-            this.txt_TenNcc.TabIndex = 6;
+            this.txt_TenNcc.Size = new System.Drawing.Size(223, 29);
+            this.txt_TenNcc.TabIndex = 3;
             // 
             // txt_diachi
             // 
-            this.txt_diachi.Location = new System.Drawing.Point(419, 116);
+            this.txt_diachi.Location = new System.Drawing.Point(538, 163);
+            this.txt_diachi.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txt_diachi.Name = "txt_diachi";
-            this.txt_diachi.Size = new System.Drawing.Size(174, 29);
+            this.txt_diachi.Size = new System.Drawing.Size(223, 29);
             this.txt_diachi.TabIndex = 5;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(20, 119);
+            this.label4.Location = new System.Drawing.Point(26, 166);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(72, 21);
             this.label4.TabIndex = 3;
@@ -178,7 +208,8 @@ namespace _3_GUI_PresentationLayer
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(304, 47);
+            this.label3.Location = new System.Drawing.Point(392, 66);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(109, 21);
             this.label3.TabIndex = 2;
@@ -187,7 +218,8 @@ namespace _3_GUI_PresentationLayer
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(20, 47);
+            this.label1.Location = new System.Drawing.Point(26, 66);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(71, 21);
             this.label1.TabIndex = 0;
@@ -196,7 +228,8 @@ namespace _3_GUI_PresentationLayer
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(17, 34);
+            this.label2.Location = new System.Drawing.Point(22, 47);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(78, 21);
             this.label2.TabIndex = 1;
@@ -204,29 +237,34 @@ namespace _3_GUI_PresentationLayer
             // 
             // txt_search
             // 
-            this.txt_search.Location = new System.Drawing.Point(15, 74);
+            this.txt_search.Location = new System.Drawing.Point(20, 103);
+            this.txt_search.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txt_search.Name = "txt_search";
-            this.txt_search.Size = new System.Drawing.Size(156, 29);
-            this.txt_search.TabIndex = 4;
+            this.txt_search.Size = new System.Drawing.Size(199, 29);
+            this.txt_search.TabIndex = 6;
             // 
             // btn_thoat
             // 
-            this.btn_thoat.Location = new System.Drawing.Point(50, 180);
+            this.btn_thoat.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btn_thoat.Location = new System.Drawing.Point(64, 252);
+            this.btn_thoat.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btn_thoat.Name = "btn_thoat";
-            this.btn_thoat.Size = new System.Drawing.Size(91, 29);
-            this.btn_thoat.TabIndex = 9;
+            this.btn_thoat.Size = new System.Drawing.Size(117, 40);
+            this.btn_thoat.TabIndex = 12;
             this.btn_thoat.Text = "Thoát";
-            this.btn_thoat.UseVisualStyleBackColor = true;
+            this.btn_thoat.UseVisualStyleBackColor = false;
             this.btn_thoat.Click += new System.EventHandler(this.btn_thoat_Click);
             // 
             // btn_search
             // 
-            this.btn_search.Location = new System.Drawing.Point(50, 135);
+            this.btn_search.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btn_search.Location = new System.Drawing.Point(64, 189);
+            this.btn_search.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btn_search.Name = "btn_search";
-            this.btn_search.Size = new System.Drawing.Size(91, 29);
+            this.btn_search.Size = new System.Drawing.Size(117, 40);
             this.btn_search.TabIndex = 11;
             this.btn_search.Text = "Tìm Kiếm";
-            this.btn_search.UseVisualStyleBackColor = true;
+            this.btn_search.UseVisualStyleBackColor = false;
             this.btn_search.Click += new System.EventHandler(this.btn_search_Click);
             // 
             // groupBox3
@@ -236,31 +274,27 @@ namespace _3_GUI_PresentationLayer
             this.groupBox3.Controls.Add(this.txt_search);
             this.groupBox3.Controls.Add(this.label2);
             this.groupBox3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.groupBox3.Location = new System.Drawing.Point(627, 12);
+            this.groupBox3.ForeColor = System.Drawing.Color.Yellow;
+            this.groupBox3.Location = new System.Drawing.Point(806, 16);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(190, 222);
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox3.Size = new System.Drawing.Size(244, 310);
             this.groupBox3.TabIndex = 2;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Chức Năng";
             // 
-            // btn_luu
-            // 
-            this.btn_luu.Location = new System.Drawing.Point(385, 180);
-            this.btn_luu.Name = "btn_luu";
-            this.btn_luu.Size = new System.Drawing.Size(91, 29);
-            this.btn_luu.TabIndex = 14;
-            this.btn_luu.Text = "Lưu";
-            this.btn_luu.UseVisualStyleBackColor = true;
-            this.btn_luu.Click += new System.EventHandler(this.btn_luu_Click);
-            // 
             // Frm_QuanLiNCC
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(825, 450);
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(1060, 630);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Frm_QuanLiNCC";
             this.Text = "Frm_QuanLiNCC";
             this.groupBox1.ResumeLayout(false);
--- DUAN1_NHOM333/3_GUI_PresentationLayer/Frm_QuanLiNCC.cs
+++ DUAN1_NHOM333/3_GUI_PresentationLayer/Frm_QuanLiNCC.cs
@@ -31,60 +31,64 @@ namespace _3_GUI_PresentationLayer
             dgv_NhaCungCap.ColumnCount = 5;
             dgv_NhaCungCap.Columns[0].Name = "Mã NCC";
             dgv_NhaCungCap.Columns[1].Name = "Tên NCC";
-            dgv_NhaCungCap.Columns[2].Name = "Số Điện Thoại";
+            dgv_NhaCungCap.Columns[2].Name = "Số ĐT";
             dgv_NhaCungCap.Columns[3].Name = "Địa Chỉ";
-            dgv_NhaCungCap.Columns[4].Name = "Trạng Thái";
+            dgv_NhaCungCap.Columns[3].Name = "Trạng Thái";
             dgv_NhaCungCap.Rows.Clear();
-            if (_iServicesQLNhaCungCap.getLstNhaCungCaps().Count < 0) return;
-            foreach (var x in _iServicesQLNhaCungCap.getLstNhaCungCaps())
+            if (_iServicesQLNhaCungCap.getLstNhaCungCap().Count < 0) return;
+            foreach (var x in _iServicesQLNhaCungCap.getLstNhaCungCap())
             {
-                dgv_NhaCungCap.Rows.Add(x.MaNhaCungCap, x.TenNhaCungCap, x.SoDienThoai, x.DiaChi, x.TrangThai == 1 ? "Hoạt Động" : "Không Hoạt Động", x.TrangThai == 0 ? "Không Hoạt Động" : "Hoạt Động");
+                dgv_NhaCungCap.Rows.Add(x.MaNhaCungCap, x.TenNhaCungCap, x.SoDienThoai, x.DiaChi, x.TrangThai == 1 ? "Hoạt Động" : "Không Hoạt Động");
             }
         }
 
         private void dgv_NhaCungCap_CellClick(object sender, DataGridViewCellEventArgs e)
         {
+            btn_Them.Enabled = false;
             var row = dgv_NhaCungCap.Rows;
             int rowindex = e.RowIndex;
-            if (rowindex >= _iServicesQLNhaCungCap.getLstNhaCungCaps().Count || rowindex < 0) return;
+            if (rowindex == _iServicesQLNhaCungCap.getLstNhaCungCap().Count || rowindex == -1) return;
             txt_MaNcc.Text = row[rowindex].Cells[0].Value.ToString();
             txt_TenNcc.Text = row[rowindex].Cells[1].Value.ToString();
             txt_SDT.Text = row[rowindex].Cells[2].Value.ToString();
             txt_diachi.Text = row[rowindex].Cells[3].Value.ToString();
-            idWhenclick = _iServicesQLNhaCungCap.getLstNhaCungCaps().Where(c => c.TenNhaCungCap == txt_TenNcc.Text).Select(c => c.MaNhaCungCap).FirstOrDefault();
+            idWhenclick = txt_MaNcc.Text;//_iServicesQLNhaCungCap.getLstNhaCungCap().Where(c => c.TenNhaCungCap == txt_TenNcc.Text).Select(c => c.MaNhaCungCap).FirstOrDefault();
         }
 
         private void btn_Them_Click(object sender, EventArgs e)
         {
-            NhaCungCap ncc = new NhaCungCap();
-            ncc.Id = _iServicesQLNhaCungCap.getLstNhaCungCaps().Max(c => c.Id) + 1;
-            ncc.MaNhaCungCap = "MNCC" + ncc.Id;
-            ncc.TenNhaCungCap = txt_TenNcc.Text;
-            ncc.SoDienThoai = txt_SDT.Text;
-            ncc.DiaChi = txt_diachi.Text;
-            ncc.TrangThai = 1;
             if (MessageBox.Show("Bạn có muốn thêm hay không?","Thông Báo",MessageBoxButtons.YesNo,MessageBoxIcon.Question)==DialogResult.Yes)
             {
-                MessageBox.Show(_iServicesQLNhaCungCap.ThemNCC(ncc));
-                LoadData();
-                clearfrom();
+                NhaCungCap ncc = new NhaCungCap();
+                ncc.Id = _iServicesQLNhaCungCap.getLstNhaCungCap().Max(c => c.Id) + 1;
+                ncc.MaNhaCungCap = "MNCC" + ncc.Id;
+                ncc.TenNhaCungCap = txt_TenNcc.Text;
+                ncc.SoDienThoai = txt_SDT.Text;
+                ncc.DiaChi = txt_diachi.Text;
+                ncc.TrangThai = 1;
+
+                MessageBox.Show(_iServicesQLNhaCungCap.ThemNCC(ncc), "Thông Báo");
+                
             }
+            LoadData();
         }
 
         private void btn_sua_Click(object sender, EventArgs e)
         {
-            NhaCungCap ncc = new NhaCungCap();
-            ncc.MaNhaCungCap = idWhenclick;
-            ncc.TenNhaCungCap = txt_TenNcc.Text;
-            ncc.SoDienThoai = txt_SDT.Text;
-            ncc.DiaChi = txt_diachi.Text;
-            ncc.TrangThai = 1;
+            btn_Them.Enabled = false;
             if (MessageBox.Show("Bạn có muốn sửa hay không?", "Thông Báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
             {
-                MessageBox.Show(_iServicesQLNhaCungCap.SuaNCC(ncc));
-                LoadData();
-                clearfrom();
+                NhaCungCap ncc = new NhaCungCap();
+                ncc.MaNhaCungCap = idWhenclick;
+                ncc.TenNhaCungCap = txt_TenNcc.Text;
+                ncc.SoDienThoai = txt_SDT.Text;
+                ncc.DiaChi = txt_diachi.Text;
+                ncc.TrangThai = 1;
+
+                MessageBox.Show(_iServicesQLNhaCungCap.SuaNCC(ncc), "Thông Báo");
+               
             }
+            LoadData();
         }
         private void clearfrom()
         {
@@ -92,6 +96,7 @@ namespace _3_GUI_PresentationLayer
             txt_TenNcc.Text = "";
             txt_SDT.Text = "";
             txt_diachi.Text = "";
+            btn_Them.Enabled = true;
         }
         private void btn_nhaplai_Click(object sender, EventArgs e)
         {
@@ -103,9 +108,9 @@ namespace _3_GUI_PresentationLayer
             DialogResult confirm;
             if (_iServicesQLNhaCungCap.Statusdata() == false)
             {
-                confirm = MessageBox.Show("Dữ liệu chưa được lưu vào database \n bạn có muốn lưu trước khi thoát?",
+                confirm = MessageBox.Show("Dữ liệu chưa được lưu. \n Bạn có muốn lưu trước khi thoát?",
                     "Thông báo",
-                    MessageBoxButtons.YesNo);
+                    MessageBoxButtons.YesNo,MessageBoxIcon.Stop);
                 if (confirm == DialogResult.Yes) _iServicesQLNhaCungCap.saveNCC();
             }
             this.Close();
@@ -113,17 +118,12 @@ namespace _3_GUI_PresentationLayer
 
         private void btn_search_Click(object sender, EventArgs e)
         {
-            dgv_NhaCungCap.ColumnCount = 5;
-            dgv_NhaCungCap.Columns[0].Name = "Mã NCC";
-            dgv_NhaCungCap.Columns[1].Name = "Tên NCC";
-            dgv_NhaCungCap.Columns[2].Name = "Số Điện Thoại";
-            dgv_NhaCungCap.Columns[3].Name = "Địa Chỉ";
-            dgv_NhaCungCap.Columns[4].Name = "Trạng Thái";
             dgv_NhaCungCap.Rows.Clear();
-            if (_iServicesQLNhaCungCap.getLstNhaCungCaps().Count < 0) return;
-            foreach (var x in _iServicesQLNhaCungCap.getLstNhaCungCaps().Where(c=>c.TenNhaCungCap.ToLower().StartsWith(txt_TenNcc.Text)))
+            if (_iServicesQLNhaCungCap.getLstNhaCungCap().Count < 0) return;
+            _iServicesQLNhaCungCap = new ServicesQLNhaCungCap();
+            foreach (var x in _iServicesQLNhaCungCap.getLstNhaCungCap().Where(c=>c.TenNhaCungCap.ToLower().StartsWith(txt_search.Text)))
             {
-                dgv_NhaCungCap.Rows.Add(x.MaNhaCungCap, x.TenNhaCungCap, x.SoDienThoai, x.DiaChi, x.TrangThai == 1 ? "Hoạt Động" : "Không Hoạt Động", x.TrangThai == 0 ? "Không Hoạt Động" : "Hoạt Động");
+                dgv_NhaCungCap.Rows.Add(x.MaNhaCungCap, x.TenNhaCungCap, x.SoDienThoai, x.DiaChi, x.TrangThai == 1 ? "Hoạt Động" : "Không Hoạt Động");
             }
         }
 
@@ -131,8 +131,9 @@ namespace _3_GUI_PresentationLayer
         {
             if (MessageBox.Show("Bạn có muốn lưu không?","Thông báo",MessageBoxButtons.YesNo,MessageBoxIcon.Question)==DialogResult.Yes)
             {
-                MessageBox.Show(_iServicesQLNhaCungCap.saveNCC());
+                MessageBox.Show(_iServicesQLNhaCungCap.saveNCC(),"Thông Báo");
             }
+            LoadData();
         }
     }
 }
